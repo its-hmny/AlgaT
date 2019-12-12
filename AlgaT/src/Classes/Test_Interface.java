6d3f806904d01322187b8056e8b52d14b58a05c5
Enea Guidi <eneag3@hotmail.com>
2019-12-12 19:35:08 +0100

    Cleaned the working tree and updated something

--- AlgaT/src/Classes/Test_Interface.java
+++
@@ -1,7 +0,0 @@
-package Classes;
-
-public interface Test_Interface extends Basic_Interface {
-    //Used to check the user input, in the Test part of the application, as you can guess
-    //returns true (if the user ha responded correctly) else false
-    boolean checkAnswer();
-}
